3f87edd08c498f4e5c9f720cebf3a4c89b277b50
unknown <you@example.com>
2024-01-23 22:38:11 +0800

    Added a pseudocode that tackles about word lenght

--- printing_even_index_characters.py
+++ printing_even_index_characters.py
@@ -6,4 +6,7 @@ word = input("Please enter your chosen word: ")
 # Print word
 print("The word you've chosen is:", word)
 
+# Get the lenght of the string
+size = len(word)
+
 # Print even index characters
