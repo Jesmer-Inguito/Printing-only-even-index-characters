699dad54c6c65d47dfa4e82dfcb4e6b1ab0c917d
unknown <you@example.com>
2024-01-23 22:23:20 +0800

    Display chosen word

--- printing_even_index_characters.py
+++ printing_even_index_characters.py
@@ -1,8 +1,9 @@
 # pseudocode
 
 # Enter word
-word = input('Please enter your chosen word: ')
+word = input("Please enter your chosen word: ")
 
 # Print word
+print("The word you've chosen is:", word)
 
 # Print even index characters
